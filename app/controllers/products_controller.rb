bcc9448c125875787a4eae921bd8f66bf5c6bf2f
Cyril Leroux <cyrilleroux@MacBook.local>
2016-04-11 22:37:32 +0200

    update Product controller and view according the Product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,15 @@
 class ProductsController < ApplicationController
 
-  PRODUCTS = [
-    {name: "product1" , category: "cat1"},
-    {name: "product2" , category: "cat1"},
-    {name: "product3" , category: "cat2"},
-    {name: "product4" , category: "cat2"},
-    {name: "product5" , category: "cat3"}
-  ]
-
   def index
     if params[:filter]
-      @products= PRODUCTS.select {|product| product[:category] == params[:filter]}
+      @products= Product.select {|product| product[:category] == params[:filter]}
     else
-      @products = PRODUCTS
+      @products = Product.all
     end
   end
 
   def show
-    @product = PRODUCTS[params[:id].to_i]
+    @product = Product.find(params[:id])
   end
 
 end
