003aa6010d3f9ff255ff45209e90588bcbc7bdd5
Cyril Leroux <cyrilleroux@MacBook.local>
2016-04-11 16:19:16 +0200

    adding search with param features for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,16 @@
 class ProductsController < ApplicationController
 
+  PRODUCTS = [
+    {name: "product1" , category: "cat1"},
+    {name: "product2" , category: "cat1"},
+    {name: "product3" , category: "cat2"},
+    {name: "product4" , category: "cat2"},
+    {name: "product5" , category: "cat3"}
+  ]
+
   def index
 
-    @products = ["pro1","pro2","pro3"]
+    @products
 
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,4 @@ class UsersController < ApplicationController
 
   end
 
-
-
 end
